f0a4b62b9395d83aa466437b1c81acef5fb38eb5
Doğan Ensar Papuçcuoğlu <ensarpabuccuoglu@gmail.com>
2025-01-21 20:20:04 +0300

    Initial Commit

---
+++ ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by Doğan Ensar Papuçcuoğlu on 21.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
