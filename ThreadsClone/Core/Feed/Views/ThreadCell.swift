2815ea96ac424dddade6998c96a0283591f1376f
Doğan Ensar Papuçcuoğlu <ensarpabuccuoglu@gmail.com>
2025-01-25 19:14:17 +0300

    Create FeedView UI

---
+++ ThreadsClone/Core/Feed/Views/ThreadCell.swift
@@ -0,0 +1,74 @@
+//
+//  ThreadCell.swift
+//  ThreadsClone
+//
+//  Created by Doğan Ensar Papuçcuoğlu on 25.01.2025.
+//
+
+import SwiftUI
+
+struct ThreadCell: View {
+    var body: some View {
+        VStack {
+            HStack(alignment: .top, spacing: 12) {
+                Image("max-verstappen")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())
+                VStack(alignment: .leading, spacing: 4) {
+                    HStack {
+                        Text("maxverstappen1")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                        Spacer()
+                        Text("10m")
+                            .font(.caption)
+                            .foregroundStyle(Color(.systemGray3))
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "ellipsis")
+                                .foregroundStyle(Color(.darkGray))
+                        }
+                    }
+                    
+                    Text("Formula 1 champion")
+                        .font(.footnote)
+                        .multilineTextAlignment(.leading)
+                    HStack(spacing: 16) {
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "heart")
+                        }
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "bubble.right")
+                        }
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "arrow.rectanglepath")
+                        }
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "paperplane")
+                        }
+                    }
+                    .padding(.vertical, 8)
+                    .foregroundStyle(.black)
+                }
+            }
+            
+        }
+        .padding()
+        Divider()
+    }
+}
+
+#Preview {
+    ThreadCell()
+}
